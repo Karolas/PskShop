de72990759a5c67db1dbf87a731419d3a2944b9e
Zivilek <zkinduryte@gmail.com>
2018-05-27 16:06:55 +0300

    almost done purchase history

---
+++ src/main/java/psk/businessLogic/purchaseHistoryLogic/purchaseHistoryUtility.java
@@ -0,0 +1,44 @@
+package psk.businessLogic.purchaseHistoryLogic;
+
+import org.primefaces.model.SortOrder;
+import psk.database.dao.OrderDAO;
+import psk.database.entities.Order;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+@Named
+@SessionScoped
+public class purchaseHistoryUtility implements Serializable {
+
+    @Inject
+    private OrderDAO orderDAO;
+
+    public int getProductsCount(String nameCriteria) {
+        return orderDAO.count(nameCriteria);
+    }
+
+//    public List<Order> getProductsPage(int first, int pageSize, String nameCriteria) {
+//        return orderDAO.getResultList(first, pageSize, nameCriteria);
+//    }
+
+    public int getProductsCountByFilters(Map<String, Object> filters){
+        return orderDAO.countWithilters(filters);
+    }
+    public List<Order> getResultList(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+        return orderDAO.getResultList(first, pageSize, sortField, sortOrder, filters);
+    }
+    public void updateProduct(Order order){
+        orderDAO.updateProduct(order);
+    }
+//    public void createProduct(Order order) {
+//        orderDAO.insertProduct(order);
+//    }
+//    public void deleteProduct(Order order) {
+//        orderDAO.deleteProduct(order.getId());
+//    }
+}
---
+++ src/main/java/psk/front/ModPurchaseHistoryFront.java
@@ -0,0 +1,93 @@
+package psk.front;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.primefaces.context.RequestContext;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+import org.primefaces.model.UploadedFile;
+import psk.businessLogic.productLogic.ProductUtility;
+import psk.businessLogic.purchaseHistoryLogic.purchaseHistoryUtility;
+import psk.database.entities.Order;
+import psk.database.entities.OrderProduct;
+import psk.database.entities.Product;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ViewScoped
+@Named
+public class ModPurchaseHistoryFront {
+
+    private byte[] bytes;
+    @Getter
+    @Setter
+    private LazyDataModel<Order> lazyModel;
+
+    @Inject
+    private purchaseHistoryUtility purchaseHistoryUtility;
+
+    @Getter
+    @Setter
+    private Order selectedOrder;
+
+    @Getter
+    @Setter
+    private UploadedFile uploadedFile;
+
+    @Getter
+    @Setter
+    private List<Order> orderList = new ArrayList<>();
+
+    @Getter
+    @Setter
+    private List<OrderProduct> orderProductList = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        lazyModel = new LazyDataModel<Order>() {
+
+            @Override
+            public List<Order> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+                lazyModel.setRowCount(purchaseHistoryUtility.getProductsCountByFilters(filters));
+                return purchaseHistoryUtility.getResultList(first, pageSize, sortField, sortOrder, filters);
+            }
+        };
+        lazyModel.setRowCount(purchaseHistoryUtility.getProductsCountByFilters(new HashMap<String, Object>()));
+    }
+
+//    public void select(boolean isNew) {
+//        if (isNew) {
+//            selectedOrder = new Order();
+//        } else {
+//            selectedOrder = this.lazyModel.getRowData();
+//        }
+//    }
+
+//    public void updateProduct() {
+//        selectedProduct.setImage(bytes);
+//        productUtility.updateProduct(selectedProduct);
+//    }
+//
+//    public void createProduct() {
+//        selectedProduct.setImage(bytes);
+//        productUtility.createProduct(selectedProduct);
+//    }
+//
+//    public void deleteProduct(){
+//        selectedProduct = this.lazyModel.getRowData();
+//        productUtility.deleteProduct(selectedProduct);
+//        this.init();
+//        this.updateTable();
+//    }
+
+    public void updateTable() {
+        RequestContext.getCurrentInstance().update("purchaseHistoryForm:lazyPurchaseHistoryItems");
+    }
+}
